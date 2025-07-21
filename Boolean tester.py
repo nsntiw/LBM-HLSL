e37217aaa403c9015366b3886436e5a168619494
nsntiw <jamiek86734@gmail.com>
2025-07-21 16:00:13 -0400

    tested all current bool funcs

--- Boolean tester.py
+++ Boolean tester.py
@@ -1,30 +1,30 @@
 class bool_op():
    @staticmethod
-   def truncate_to_binary(a):
+   def truncate_to_binary(a): #OK
       a = max(min(1, a), 0)
       return a
 
    @staticmethod
-   def ADD(a1, a2):
+   def ADD(a1, a2): #OK
       return 1 if a1+a2 == 1 else 0
 
-   @staticmethod
+   @staticmethod #OK
    def SUBTRACT(a1, a2):
       return max(a1-a2,0)
 
    @staticmethod
-   def AND(a1, a2):
+   def AND(a1, a2): #OK
       return 1 if a1==a2==1 else 0
 
    @staticmethod
-   def OR(a1, a2):
+   def OR(a1, a2): #OK
       return bool_op.truncate_to_binary(a1+a2)
 
    @staticmethod
-   def NOT(a):
+   def NOT(a): #OK
       return bool_op.truncate_to_binary(abs(1-a))
 
-class test():
+class TESTER():
    def __init__(self, operation, num_bits):
       self.operation = operation
       self.num_bits = num_bits
@@ -36,11 +36,3 @@ class test():
 
 opA, opB = [1]*4, [1]*4
 print(opA, opB)
-
-print(bool_op.OR(0,0))
-print(bool_op.OR(0,1))
-print(bool_op.OR(1,0))
-print(bool_op.OR(1,1))
-
-print(bool_op.NOT(0))
-print(bool_op.NOT(1))
